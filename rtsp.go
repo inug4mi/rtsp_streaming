6e759becada494e38c1c40f5fc12c1b326848d56
inug4mi <carlos.casas1@udea.edu.co>
2026-06-12 16:28:50 -0500

    low latency

--- rtsp.go
+++ rtsp.go
@@ -8,23 +8,41 @@ import (
 	"time"
 )
 
+/*
+	Parametros ffmpeg
+
+-f dshow	Usa DirectShow, la interfaz de Windows para acceder a cámaras y micrófonos.
+-rtbufsize 100M	Reserva un buffer de 100 MB para evitar pérdida de frames cuando la captura va más rápido que el procesamiento.
+-framerate 30	Captura 30 imágenes por segundo (30 FPS).
+-video_size 640x480	Resolución del video. Más resolución = más calidad pero más consumo de CPU y red.
+-i video="HD camera "	Dispositivo de entrada. En este caso la webcam llamada "HD camera".
+-c:v libx264	Codifica el video usando H.264 mediante la biblioteca x264.
+-preset ultrafast	Usa la configuración más rápida posible del codificador para reducir la latencia.
+-tune zerolatency	Optimiza el encoder para streaming en tiempo real, reduciendo buffers internos.
+-g 6	Genera un frame clave (I-Frame) cada 6 frames. Ayuda a que los clientes se conecten más rápido y reduce la latencia.
+-bf 0	Desactiva B-Frames. Mejora la latencia porque evita que FFmpeg tenga que esperar frames futuros.
+-pix_fmt yuv420p	Convierte el formato de color a uno compatible con casi todos los reproductores y navegadores.
+-rtsp_transport tcp	Usa TCP para transportar RTSP. Más confiable que UDP aunque puede agregar algo de latencia.
+-f rtsp	Indica que el formato de salida será RTSP.
+rtsp://127.0.0.1:8554/webcam	Dirección donde se publica el stream en MediaMTX.
+*/
 func main() {
 
 	// FFmpeg (publicador RTSP)
 	ffmpeg := exec.Command(
 		"ffmpeg",
 		"-f", "dshow",
-		"-rtbufsize", "100M",
-		"-framerate", "15",
+		"-rtbufsize", "10M",
+		"-framerate", "30",
 		"-video_size", "640x480",
 		"-i", `video=HD camera `,
-		"-c:v", "libx264",
-		"-preset", "ultrafast",
-		"-tune", "zerolatency",
-		"-g", "15",
-		"-bf", "0",
+		"-c:v", "libx264", // codificacion H.264
+		"-preset", "ultrafast", // es la configuración más rápida posible del codificador
+		"-tune", "zerolatency", // Optimiza el encoder para streaming en tiempo real
+		"-g", "30", // cada x frames, genera un I-Frame
+		"-bf", "0", // B-Frames
 		"-pix_fmt", "yuv420p",
-		"-rtsp_transport", "tcp",
+		"-rtsp_transport", "udp",
 		"-f", "rtsp",
 		"rtsp://127.0.0.1:8554/webcam",
 	)
